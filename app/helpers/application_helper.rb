20d69000c5c09b3f7daf3f50e22adf1e6d3e264c
Mark <mark@bhalash.com>
2015-11-17 22:21:48 +0000

    Added method to retrieve avatar from the avatar.io service.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  require 'digest/md5'
+
+  def adorable_avatar(user, size = 75)
+    # Send out MD5 of email for avatar. No need to use user info.
+    # Will give consistent avatars per run.
+    avatar = Digest::MD5.hexdigest(user.email)
+
+    # Question: Is this insertion correct?
+    src = "http://api.adorable.io/avatars/#{size}/#{avatar}.png"
+    alt = "#{user.login}'s handsome avatar."
+
+    link_to image_tag(src, alt: alt), user_path(user)
+  end
 end
