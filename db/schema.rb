8de49a0c2f0304690b76c9f8c0d170080d685484
Mark <mark@bhalash.com>
2015-11-17 14:43:53 +0000

    Added tweets schema and migrate file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151117101327) do
+ActiveRecord::Schema.define(version: 20151117134636) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.text     "content",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "user_sessions", force: :cascade do |t|
     t.string   "session_id", null: false
