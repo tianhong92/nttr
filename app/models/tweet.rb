07a6889c5a53b8ba954ffff6648e224cd55e5903
Mark <mark@bhalash.com>
2015-11-17 16:28:48 +0000

    Added validation for tweets.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,6 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
+
+  validates :content, presence: true, length: { maximum: 140, message: 'Your nttr is too long!' }
+  validates :content, uniqueness: { scope: :content, within: :minute, message: 'You\'ve already nttrd about this!' }
 end
