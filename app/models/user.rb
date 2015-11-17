7366ee965ea4f9d79b093f63f816868127395c76
Mark <mark@bhalash.com>
2015-11-17 16:29:30 +0000

    Expanded nested resoueces.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :tweets
+  has_many :tweets, dependent: :destroy
+  accepts_nested_attributes_for :tweets, allow_destroy: true
 
   acts_as_authentic do |opts|
     opts.merge_validates_length_of_password_field_options({ 
