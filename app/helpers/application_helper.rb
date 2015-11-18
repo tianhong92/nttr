239fbaf5c6ac477fca674218c8a84a0efcd9173c
Mark <mark@bhalash.com>
2015-11-18 17:19:59 +0000

    Added CSS classes to avatar output.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,15 @@
 module ApplicationHelper
-  def adorable_avatar(user, size = 75)
+  def adorable_avatar(user, size = 75, classes = '')
     # Send out MD5 of email for avatar. No need to use user info.
     # Will give consistent avatars per run.
     avatar = user.login_md5
 
     # Question: Is this insertion correct?
     src = "http://api.adorable.io/avatars/#{size}/#{avatar}.png"
-    alt = "#{user.login}'s handsome avatar."
+    alt = user.nicename
     size = "#{size}x#{size}"
+    classes = classes.prepend('adorable-avatar ')
 
-    link_to image_tag(src, alt: alt, size: size), user_path(user)
+    link_to image_tag(src, alt: alt, size: size, class: classes), user_path(user)
   end
 end
