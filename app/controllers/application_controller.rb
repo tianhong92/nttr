ddae376f94883730e0a5fce4492e278d76526cc3
Mark <mark@bhalash.com>
2015-11-18 11:18:35 +0000

    Create empty session.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :new_session
 
   # Helper methods are made available to the application view.
   helper_method :current_user_session, :current_user
 
   private
+    def new_session
+      @user_session = UserSession.new
+    end
+
     def current_user_session
       return @current_user_session if defined?(@current_user_session)
       @current_user_session = UserSession.find
