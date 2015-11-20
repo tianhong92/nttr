9159e6542633503a424a52b7710966dee30249a3
Mark <mark@bhalash.com>
2015-11-20 10:09:34 +0000

    Changed selector variables so as to be less samey

--- app/assets/javascripts/nttr.js
+++ app/assets/javascripts/nttr.js
@@ -3,21 +3,23 @@ $(document).ready(function() {
     // Tweet selectors.
     //
 
-    var tweets = {
-        form: {
-            id: '#new_tweet',
-            counter: '.broadcast__counter',
-            input: '#tweet_content',
-            errors: '.create__errors'
-        },
-        tweet: '.tweet',
+    var broadcast = {
+        id: '#new_tweet',
+        input: '#tweet_content',
+        counter: '.broadcast__counter',
+        errors: '.broadcast__errors'
+    }
+
+    var tweet = {
+        type: '.tweet',
+        content: '.tweet__content',
         list: '.tweet-list',
         del: '.tweet__delete',
         err: 'error--highlight',
         max: 140
-    };
+    }
 
-    // AJAX submit new tweets.
+    // AJAX submit new tweet.
     var animationTime = 400;
 
     //
@@ -27,7 +29,7 @@ $(document).ready(function() {
     $.fn.tweetLengthCounter = function(args) {
         args = $.extend({}, {
             counter: '',
-            max: tweets.max,
+            max: tweet.max,
             tolerance: 15,
             warnClass: 'text--error',
             dispClass: 'display--none'
@@ -52,13 +54,13 @@ $(document).ready(function() {
     };
 
     // Display the length of your tweet below the message.
-    $(tweets.form.input).tweetLengthCounter({
-        counter: tweets.form.counter
+    $(broadcast.input).tweetLengthCounter({
+        counter: broadcast.counter
     });
 
     function clearErrors() {
-        $(tweets.form.input).removeClass(tweets.err);
-        $(tweets.form.errors).empty();
+        $(broadcast.input).removeClass(tweet.err);
+        $(broadcast.errors).empty();
     }
 
     //
@@ -75,13 +77,13 @@ $(document).ready(function() {
         return this;
     }
 
-    $(tweets.form.id).maxTweetLength(tweets.max, tweets.form.input);
+    $(broadcast.id).maxTweetLength(tweet.max, broadcast.input);
 
     // 
     // Send tweet on ctrl/command + enter.
     //
 
-    $(tweets.form.input).on('keydown', function(keys) {
+    $(broadcast.input).on('keydown', function(keys) {
         if (keys.ctrlKey || keys.metaKey && keys.keyCode === 13) {
             $(this).closest('form').submit();
         }
@@ -94,32 +96,31 @@ $(document).ready(function() {
     //
 
     function removeHook(err, data, state, xhr) {
-        $(this).closest(tweets.tweet).fadeOut(animationTime, function() {
+        $(this).closest(tweet.tweet).fadeOut(animationTime, function() {
             $(this).remove();
         });
     }
 
-    $(tweets.del).on('ajax:success', removeHook);
+    $(tweet.del).on('ajax:success', removeHook);
 
     //
     // AJAX add tweet callback.
     //
     
-    $(tweets.form.id).on('ajax:success', function(err, data) {
+    $(broadcast.id).on('ajax:success', function(err, data) {
         // Clear input and reset errors upon submit.
         clearErrors();
-        $(tweets.form.input).val('');
+        $(broadcast.input).val('');
 
         // Hide new tweet before it is prepended to the roster.
         $(data)
             .hide()
-            .prependTo(tweets.list)
-            .fadeIn(animationTime).find(tweets.del)
+            .prependTo(tweet.list)
+            .fadeIn(animationTime).find(tweet.del)
             .on('ajax:success', removeHook);
     }).on('ajax:error', function(err, xhr) {
-        $(tweets.form.input).addClass(tweets.err);
-        // console.log(JSON.parse(xhr.responseText));
-        $(tweets.form.errors).append($(xhr.responseText));
+        $(broadcast.input).addClass(tweet.err);
+        $(broadcast.errors).append($(xhr.responseText));
         console.log(xhr);
     });
 });
