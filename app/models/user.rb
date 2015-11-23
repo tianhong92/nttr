8c1783d28a6d9edbc7078122d813fb9733ee884c
Mark <mark@bhalash.com>
2015-11-23 12:46:46 +0000

    Added login authentication by username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ActiveRecord::Base
       message: 'Your password is too short!'
     })
   end
+
+  private
+    def self.find_by_login_or_email(login)
+      find_by_login(login) || find_by_email(login)
+    end
 end
