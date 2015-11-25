63313ac02deba04475e70f861913c01ec4aacbbb
Mark <mark@bhalash.com>
2015-11-25 12:11:40 +0000

    Moved spec includes to appropriate files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,4 @@
 RSpec.configure do |config|
-  require 'capybara/rspec'
-  config.include Capybara::DSL
-
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
