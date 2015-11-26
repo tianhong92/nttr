1ebf2d5cf92fccfddb87db2d2609be977235e24d
Mark <mark@bhalash.com>
2015-11-26 16:46:25 +0000

    Moved login test function to helper file

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -5,3 +5,11 @@ end
 class Test::Unit::TestCase
   include FactoryGirl::Syntax::Methods
 end
+
+def login_user(test_user)
+    within '#login' do
+      fill_in 'Username or email', with: test_user.email
+      fill_in 'Password', with: test_user.password
+      click_button 'Log in'
+    end
+end
