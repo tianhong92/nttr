00da42bbec8fe7bcaa910153efadd27760d68da0
Mark <mark@bhalash.com>
2015-11-26 16:47:57 +0000

    Added RSpec config

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,17 @@ module Authy
 
     # Filter passwords fields from logs.
     config.filter_parameters += [:password, :password_confirmation]
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_spec: false,
+        controller_specs: true,
+        request_specs: true
+
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
   end
 end
