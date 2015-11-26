fc650ddeff18f9a12b61dda0cf40acb467a96ae1
Mark <mark@bhalash.com>
2015-11-26 16:48:40 +0000

    Added FactoryGirl factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,18 @@
 FactoryGirl.define do
   factory :user do
+    password_str = 'squidloverhothot'
+    salt = Authlogic::Random.hex_token
+
+    id 1  
     login 'squid'
     email 'squidlover@bhalash.com'
     nicename 'Squid Lover'
-    password 'squidloverhothot'
-    password_confirmation 'squidloverhothot'
+
+    password password_str 
+    password_confirmation password_str 
+    
+    password_salt salt 
+    persistence_token Authlogic::Random.hex_token
+    crypted_password Authlogic::CryptoProviders::Sha512.encrypt(password_str + salt)
   end
 end
