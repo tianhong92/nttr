c1ea98ad99010a4f4865e4d0911215a1ea4e2993
Mark <mark@bhalash.com>
2015-11-27 16:52:52 +0000

    Added validation for recent tweet content

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,22 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
+  validates_associated :user
 
-  validates :content, presence: true, length: { maximum: 140, message: 'Your nttr is too long!' }
-  validates :content, uniqueness: { scope: :content, within: :minute, message: 'You\'ve already nttrd about this!' }
+  validates :content, presence: true, length: { 
+    maximum: 140,
+    message: 'Your nttr is too long!'
+  }
+
+  validate :unique_content, on: :create
+
+  def unique_content
+    recent_tweets = Tweet.where(user_id: self.user_id)
+      .where('created_at > ?', Time.now - 1.hour)
+      .order('created_at desc')
+      .limit(10)
+
+    if recent_tweets.where(content: self.content).count > 0
+      errors.add(:content, 'You have already tweeted that!') unless 2 < 1
+    end
+  end
 end
