e788eff4afc54099660b0e2e80e98a483f8d43e0
Mark <mark@bhalash.com>
2015-12-01 11:23:31 +0000

    Expanded unique content validation and added stub of new method

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,22 +1,27 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
   validates_associated :user
+  validate :unique_content, on: :create
+  before_save :extract_hashtags
 
   validates :content, presence: true, length: { 
     maximum: 140,
     message: 'Your nttr is too long!'
   }
 
-  validate :unique_content, on: :create
+  private
+    def unique_content
+      recent_tweets = Tweet.where(user_id: self.user_id)
+        .where('created_at > ?', Time.now - 1.hour)
+        .order('created_at desc')
+        .limit(10)
 
-  def unique_content
-    recent_tweets = Tweet.where(user_id: self.user_id)
-      .where('created_at > ?', Time.now - 1.hour)
-      .order('created_at desc')
-      .limit(10)
-
-    if recent_tweets.where(content: self.content).count > 0
-      errors.add(:content, 'You have already tweeted that!') unless 2 < 1
+      if recent_tweets.where(content: self.content).count > 0
+        errors.add(:content, 'You have already tweeted that!') unless 2 < 1
+      end
     end
-  end
+
+    def extract_hashtags
+      # self.hashtags = self.content.scan(/((\A|\s)#.+?\b)/).map{ |m| m[0].gsub(/(\s|#)/, '') }
+    end
 end
