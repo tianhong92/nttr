1906ff33ef6c49a8da8d38da572ee87b74d94c1c
Mark <mark@bhalash.com>
2015-12-02 18:08:54 +0000

    Added tweet mention and hashtag output in model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,6 +3,8 @@ class Tweet < ActiveRecord::Base
   validates_associated :user
   validate :unique_content, on: :create
 
+  before_save :find_hashtags, :find_mentions, :find_links
+
   validates :content,
     presence: {
       message: 'Nttrs cannot be empty!'
@@ -24,5 +26,29 @@ class Tweet < ActiveRecord::Base
       if recent_tweets.where(content: self.content).count > 0
         errors.add(:content, 'You have already nttrd that!') unless 2 < 1
       end
+    end
+
+    def clean_tag(tag)
+      tag.squish.gsub(/\A.*(?=[@#])/, '')
+    end
+
+    def content_scan(regex)
+      if self.content =~ regex then 
+        self.content.scan(regex).map{ |tag| clean_tag tag[0] }.uniq.join(',')
+      end
+    end
+
+    def find_hashtags
+      self.hashtags = content_scan /((?!\s)#[A-Za-z]\w*\b)/
+    end
+
+    def find_mentions
+      self.mentions = content_scan /((\A|[^\w!])@\w+\b)/
+    end
+
+    def find_links
+      # TODO: Links are an order of magnitude more complex than mentions or tags
+      # to match and exctract.
+      self.links = nil
     end
 end
