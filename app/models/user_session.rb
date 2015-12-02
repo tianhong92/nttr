e4f7b8120eb09959fecec928b499c8e76d201274
Mark <mark@bhalash.com>
2015-12-02 12:55:46 +0000

    Overhauled user session error and information messages

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,3 +1,10 @@
 class UserSession < Authlogic::Session::Base
+  # Authlogic handles session validation internally. Custom error messages have
+  # been input in:
+  #
+  #   config/locales/en.yml
+  #
+  # A ist of complete error messages can be found at: 
+  # See: http://www.rubydoc.info/github/binarylogic/authlogic/Authlogic/I18n
   find_by_login_method :find_by_login_or_email
 end
