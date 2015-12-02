18c2ad85a1ad8594a39f5e9b5ad99f20c9a317f1
Mark <mark@bhalash.com>
2015-12-02 10:56:24 +0000

    Overhauled validation
    * Moved validation back to Authlogic.
    * Ported previous validations to Authlogic’s methods.
    * Added password and password confirmation validation messages.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,50 +2,37 @@ class User < ActiveRecord::Base
   has_many :tweets, dependent: :destroy
   accepts_nested_attributes_for :tweets, allow_destroy: true
   before_save :downcase_login, :create_login_md5
-  
-  validates :login,
-    presence: true,
-    length: {
-      # Twitter allows usernames to contian any word char with a minimum length
-      # of 1. These short names are considered to be desireable and can sell
-      # for many monies.
+
+  acts_as_authentic do |opts|
+    #
+    # Login
+    #
+
+    opts.merge_validates_format_of_login_field_options({
+      # Twitter allows usernames to contian any word char (A-Za-z0-9_) with a
+      # minimum length of 1. These short names are considered to be desireable
+      # and can sell for many monies.
       #
       # @_, @3, @_____, @______1 are all valid (and used) Twitter usernames.
       #
       # See: http://goo.gl/FMdLB0
+      with: /\A\w+\z/,
+      message: 'Usernames may contain only letters, underscores or numbers.'
+    })
+
+    opts.merge_validates_length_of_login_field_options({
+      # Twitter username length is inclusively in 1 - 15 characters.
+      # See: https://support.twitter.com/articles/14609
       in: 1..15,
       too_short: 'Usename must be at least 1 character long.',
       too_long: 'Username may be no more than 15 characters long.'
-    },
-    format: {
-      with: /\A\w+\z/,
-      message: 'Usernames may contain only letters, underscores or numbers.'
-    }
+    })
 
-  validates :password,
-    presence: true,
-    confirmation: true,
-    length: {
-      in: 8..100,
-      too_short: 'Password must be at least 8 characters long.',
-      too_long: 'Password may be no more than 100 characters long.'
-    }
-    # format: {
-    #  # I lost two hours of my life to this line. :|
-    #  # with: /\A3\z/
-    # }
+    #
+    # Email 
+    #
 
-  validates :password_confirmation, 
-    presence: true
-
-  validates :email,
-    presence: true,
-    length: {
-      in: 6..40,
-      too_short: 'Email must be at least 6 characters long.',
-      too_long: 'Email may be no more than 40 characters long.'
-    },
-    format: {
+    opts.merge_validates_format_of_email_field_options({
       # I've been through this in practice. Email regex is a nightmare unless
       # you KISS. This enforces *at least* a@t.co (which is a valid email
       # associated with the Twitter company).
@@ -55,9 +42,37 @@ class User < ActiveRecord::Base
       # See: http://davidcel.is/posts/stop-validating-email-addresses-with-regex
       # See: https://regex101.com/r/jS2rV9/1
       with: /\A.+@.+\..+\z/,
-      message: 'Email must be in the format of foo@bar.tld.'
-    }
+      message: 'Email must be in the format of \'foo@bar.tld\'.'
+    })
 
+    opts.merge_validates_length_of_email_field_options({
+      in: 6..40,
+      too_short: 'Email must be at least 6 characters long.',
+      too_long: 'Email may be no more than 40 characters long.'
+    })
+
+    #
+    # Password
+    #
+
+    opts.merge_validates_length_of_password_field_options({
+      # Authlogic's default password is 4 characters.
+      minimum: 8,
+      message: 'Password must be at least 8 characters long.'
+    })
+
+    opts.merge_validates_confirmation_of_password_field_options({
+      # Validate presence of password confirmation. Defaults to an unhelpful
+      # 'password must be n characters' message.
+      message: 'Input passwords do not match.'
+    })
+
+    opts.merge_validates_length_of_password_confirmation_field_options({
+      # By default this will repeat the message in the above method.
+      message: 'You must confirm your password.'
+    })
+  end
+    
   def to_param
     login
   end
