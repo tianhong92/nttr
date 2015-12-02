970283ed3315f1d0f0aa7d7a6697391e53439033
Mark <mark@bhalash.com>
2015-12-02 17:31:46 +0000

    Removed commented out code

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -3,7 +3,7 @@ module TweetsHelper
     # Change 'about 16 hours' to '16h'
     # TODO: Fix typo in "less than a minute ago"
     nouns = {hour: 'h', minute: 'm', day: 'd', month: 'm', year: 'y'}
-    time = time_ago_in_words(tweet.created_at, include_seconds: true).to_s # .gsub('about ', '')
+    time = time_ago_in_words(tweet.created_at, include_seconds: true).to_s 
 
     nouns.each do |key, value|
       time.gsub!(Regexp.new("(\\s?)#{key.to_s}(s?)"), value)
