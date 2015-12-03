02ae9d0f1e7da065ec1015092047991f7dc5a3b8
Mark <mark@bhalash.com>
2015-12-03 11:05:50 +0000

    Refactored relative time function.

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,15 +1,9 @@
 module TweetsHelper
-  def tweet_relative_time(tweet)
+  def tweet_relative_time(timestamp)
     # Change 'about 16 hours' to '16h'
-    # TODO: Fix typo in "less than a minute ago"
-    nouns = {hour: 'h', minute: 'm', day: 'd', month: 'm', year: 'y'}
-    time = time_ago_in_words(tweet.created_at, include_seconds: true).to_s 
-
-    nouns.each do |key, value|
-      time.gsub!(Regexp.new("(\\s?)#{key.to_s}(s?)"), value)
-    end
-
-    time
+    time_regex = /(about|\s|(econd|inute|our)s?)/
+    timestamp = time_ago_in_words(timestamp, include_seconds: true).to_s 
+    timestamp.gsub!(time_regex, '')
   end
 
   # This set of methods render nttr @user mentions and #hastags into clickable
