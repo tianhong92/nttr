b67d15aeace0f18b8b3055c90b09edddb789da15
Mark <mark@bhalash.com>
2015-12-07 15:54:04 +0000

    Added methods to wrap around authenticated actions, and to log in/out.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,8 @@ RSpec.configure do |config|
 end
 
 def spawn_test_user(name = 'birdman')
+  # This method is not used, but is retained for illustration of this way for
+  # the creation of dummy users.
   password = "ilove#{name.delete(' ').split(//).shuffle!.join}"
   salt = Authlogic::Random.hex_token
 
@@ -23,4 +25,23 @@ def spawn_test_user(name = 'birdman')
     # crypted_password: Authlogic::CryptoProviders::Sha512.encrypt(password + salt),
     # persistence_token: Authlogic::Random.hex_token
   )
+end
+
+def log_in_as(user)
+  within '#login' do
+    fill_in 'user_session_login', with: user.email
+    fill_in 'user_session_password', with: user.password
+    click_button 'user_session_submit'
+  end
+end
+
+def log_out
+  session = UserSession.find
+  session.destroy
+end
+
+def as_user(user)
+  log_in_as(user)
+  yield
+  log_out
 end
