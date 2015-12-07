6be6022362fa4a198b42243c4fb2626f28ad20ac
Mark <mark@bhalash.com>
2015-12-07 15:54:34 +0000

    Removed duplicate of spec_helper function.

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -5,11 +5,3 @@ end
 class Test::Unit::TestCase
   include FactoryGirl::Syntax::Methods
 end
-
-def login_user(test_user)
-    within '#login' do
-      fill_in 'Username or email', with: test_user.email
-      fill_in 'Password', with: test_user.password
-      click_button 'Log in'
-    end
-end
